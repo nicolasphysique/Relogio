a299cddd0ef3e1ad2ee316ae3e60ba94358efdc3
nicolasphysique <nicolasphysiquebr@gmail.com>
2023-08-01 23:47:05 -0300

    primeiro commit

---
+++ modelo.js
@@ -0,0 +1,27 @@
+function updateClock() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+  
+    document.getElementById('horas').textContent = hours;
+    document.getElementById('minutos').textContent = minutes;
+    document.getElementById('segundos').textContent = seconds;
+  }
+  
+setInterval(updateClock, 1000);
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
